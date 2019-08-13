45f35de1942d793dbd8fad787642874918a2e4f4
Matthew Spector <mspector@verizonmedia.com>
2019-08-13 00:14:16 -0400

    Updates

--- precache-manifest.60c1c3fc06588d1ec64f47d37810140d.js
+++ precache-manifest.60c1c3fc06588d1ec64f47d37810140d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ae71b1b57fbaa86ae8908e101eadf4d",
+    "revision": "3ec933200ffcdb917246f507d211b32e",
     "url": "/lambda/index.html"
   },
   {
-    "revision": "82691373463b86d9fb1f",
-    "url": "/lambda/static/css/main.9542cce2.chunk.css"
+    "revision": "a95486efc25e8958fbf7",
+    "url": "/lambda/static/css/main.dd5fd9b1.chunk.css"
   },
   {
     "revision": "a2215a762b3a8c9ea81a",
     "url": "/lambda/static/js/2.70b4349e.chunk.js"
   },
   {
-    "revision": "82691373463b86d9fb1f",
-    "url": "/lambda/static/js/main.8a8d7daf.chunk.js"
+    "revision": "a95486efc25e8958fbf7",
+    "url": "/lambda/static/js/main.28d4535c.chunk.js"
   },
   {
     "revision": "5c2552d1676545e92482",
